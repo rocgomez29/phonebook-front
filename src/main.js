421283acb2a46d43a9870dcff7c40fbbed17f7a3
rocgomez29 <97193118+rocgomez29@users.noreply.github.com>
2022-01-13 14:14:37 -0400

    Idenatcion y elimicacion de warnings

--- src/main.js
+++ src/main.js
@@ -5,18 +5,17 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App'
 import router from './router'
 
-import VueAxios from 'vue-axios';
-import axios from 'axios';
+import VueAxios from 'vue-axios'
+import axios from 'axios'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
-Vue.use(VueAxios, axios);
+Vue.use(VueAxios, axios)
 
 /* eslint-disable no-new */
 new Vue({
